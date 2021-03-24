263271d99e8418402506dcead7589391c91fe67c
zzo <xcxcxz@naver.com>
2021-03-24 12:34:50 +0900

    mvc+mybatis를 연동한 게시판 만들기

---
+++ first/src/main/java/com/xcxcxz/first/doamin/BoardVO.java
@@ -0,0 +1,87 @@
+package com.xcxcxz.first.doamin;
+
+import java.sql.Timestamp;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("BoardVO")
+public class BoardVO {
+	private int seq;
+	private String title;
+	private String content;
+	private String writer;
+	private String password;
+	private Timestamp regDate;
+	private int cnt;
+	
+	public BoardVO() {}
+	
+	public BoardVO(String title, String content, String writer, String password) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.writer = writer;
+		this.password = password;
+		this.cnt = 0;
+		
+	}
+
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Timestamp getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Timestamp regDate) {
+		this.regDate = regDate;
+	}
+
+	public int getCnt() {
+		return cnt;
+	}
+
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	
+	
+
+}
